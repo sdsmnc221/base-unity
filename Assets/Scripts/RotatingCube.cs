16a545b88bd58c9cda5c610944ec22e93946af19
An <antr.2201@gmail.com>
2020-02-12 14:33:13 +0100

    first

---
+++ Assets/Scripts/RotatingCube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCube : MonoBehaviour
+{
+    [SerializeField] private float speed = 22f;
+
+    void FixedUpdate()
+    {
+        float angle = Time.deltaTime * speed;
+        transform.Rotate(angle, angle, angle);
+    }
+}
+
+
